b3ad3494fd461db447f28a9e4158c44e7a7c1ece
Karthik-D <karthikdesingu2000@gmail.com>
2021-07-23 01:53:54 +0530

    Server main code for connection

--- Ex2-TCP/A_EchoServer/network.h
+++ Ex2-TCP/A_EchoServer/network.h
@@ -4,6 +4,8 @@
 #define CLIENT_PORT 8000
 #define BACKLOG_LIMIT 5
 #define LOCALHOST_IP "127.0.0.1"
+#define ADDRESS_FAMILY AF_INET
+#define ADDRESS_BUFFER_SIZE 30
 
 /*	
 Use BLOCKING sockets (default configuration)
@@ -16,7 +18,7 @@ int make_socket(){
 	// AF_INTER specifies IPv4
 	// SOCK_STREAM specifies two-way byte-stream
 	// 0 selects default protocol
-	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	int sock_fd = socket(ADDRESS_FAMILY, SOCK_STREAM, 0);
 	if (sock_fd == -1){
 		return -2;    // Could not create socket
 	}
@@ -26,14 +28,14 @@ int make_socket(){
 int bind_server_socket(int sock_fd){
 	struct sockaddr_in bind_address;
 	// Set family to IPv4
-	bind_address.sin_family = AF_INET;
+	bind_address.sin_family = ADDRESS_FAMILY;
 	// Set port in network byte-order to a non-privileged port (>1023)
 	bind_address.sin_port = htons(SERVER_PORT);
 	// Set address to 0.0.0.0 to connect to bind to all local interfaces
 	bind_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	// Convert to generic socket address format and bind
 	if (!bind(sock_fd, (struct sockaddr *)&bind_address, sizeof(bind_address))){
-		return 1;    // Success
+		return 0;    // Success
 	}
 	else{
 		return -3;   // Could not bind server-socket
@@ -44,23 +46,23 @@ int bind_server_socket(int sock_fd){
 int connect_server(int sock_fd){
 	struct sockaddr_in bind_address;
 	// Set family to IPv4
-	bind_address.sin_family = AF_INET;
+	bind_address.sin_family = ADDRESS_FAMILY;
 	// Set port in network byte-order to a non-privileged port (>1023)
 	bind_address.sin_port = htons(CLIENT_PORT);
 	// Set address to 127.0.0.1 to loopback to same host
 	bind_address.sin_addr.s_addr = inet_addr(LOCALHOST_IP);
 	// Convert to generic socket address format and bind
 	if (!connect(sock_fd, (struct sockaddr *)&bind_address, sizeof(bind_address))){
-		return 1;    // Success
+		return 0;    // Success
 	}
 	else{
 		return -5;   // Could not connect to the local server
 	}
 }
 
-int initiate_listening(int sock_fd){
+int initiate_listen(int sock_fd){
 	if (!listen(sock_fd, BACKLOG_LIMIT)){
-		return 1;    // Success
+		return 0;    // Success
 	}
 	else{
 		return -4;   // Could not initiate listening
@@ -68,11 +70,19 @@ int initiate_listening(int sock_fd){
 }
 
 // BLOCKING FUNCTION
-int accept_client(int sock_fd){
-	// Address of client is not required
-	client_sock_fd = accept(sock_fd, NULL, NULL);
+int accept_client(int sock_fd, struct sockaddr_in *client_addr, int *client_addr_len){
+	// Address of client is registered
+	client_sock_fd = accept(sock_fd, (sockaddr*)client_addr, client_addr_len);
 	if (client_sock_fd == -1){
 		return -5;   // Did not connect to a client
 	}
+	else if (*client_addr_len > sizeof(struct_sockaddr_in)){
+		*client_addr_len = -1;    // Non-Fatal Warning: Client address was truncated to fit in buffer
+	}
 	return cient_sock_fd;
+}
+
+
+void destroy_socket(int sock_fd){
+	close(sock_fd);
 }
---
+++ Ex2-TCP/A_EchoServer/server.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "network.h"
+
+void main(){
+	
+	int self_socket = make_socket();
+	if(self_socket<0){
+		printf("\nCould not create socket. Retry!\n");
+		return;
+	}
+
+	if (bind_server_socket(self_socket)<0){
+		printf("\nCould not bind server socket. Retry!\n");
+		destroy_socket(self_socket);
+		return;
+	}
+
+	if (initiate_listen(self_socket)<0){
+		printf("\nCould not listen on server socket. Retry!\n");
+		destroy_socket(self_socket);
+		return;
+	}
+	else{
+		printf("\nServer listening for connections from all local interfaces...\n");
+	}
+
+	struct sockaddr_in *client_addr = malloc(sizeof(struct sockaddr_in));
+	int client_addr_len = sizeof(struct sockaddr_in);
+	// BLOCKING routine to accept a client
+	client_socket = accept_client(self_socket, client_addr, &client_addr_len);
+	if (client_socket<0){
+		printf("\nError when connecting to client. Retry!\n");
+		destroy_socket(self_socket);
+		return;
+	}
+	else if(client_addr_len == -1){
+		printf("Client connected.\nCould not read address\n");
+	}
+	else{
+		char *client_addr_ip_str = (char*)malloc(sizeof(char)*ADDRESS_BUFFER_SIZE);
+		inet_ntop(ADDRESS_FAMILY, client_addr->sin_addr, client_addr_ip_str, ADDRESS_BUFFER_SIZE);
+		client_addr_port = (int)ntohs(client_addr->sin_port);
+		if (client_addr_ip_str == NULL) {
+			printf("Client connected.\nCould not read address\n");
+		}
+		else{
+			printf("Connected to Client (%s:%d)", client_addr_ip_str, client_addr_port);
+		}
+	}
+
+
+	
+}
