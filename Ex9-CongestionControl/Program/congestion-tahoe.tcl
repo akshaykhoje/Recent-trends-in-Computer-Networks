0b9565c93874297799103272bf8dc497a459d631
Karthik-D <karthikdesingu2000@gmail.com>
2021-10-10 07:36:26 +0530

    Tahoe done

--- Ex9-CongestionControl/Program/congestion-tahoe.tcl
+++ Ex9-CongestionControl/Program/congestion-tahoe.tcl
@@ -29,9 +29,15 @@ set n2 [$ns node]
 $ns duplex-link $n0 $n1 10Mb 10ms DropTail
 $ns duplex-link $n1 $n2 2Mb 10ms DropTail
 
+#Set Queue Size of link (n1-n2) to 5
+$ns queue-limit $n1 $n2 5
+
 #Give node position (for NAM)
 $ns duplex-link-op $n0 $n1 orient right-up   
 $ns duplex-link-op $n1 $n2 orient right-down 
+
+#Monitor the queue for link (n2-n3). (for NAM)
+$ns duplex-link-op $n1 $n2 queuePos 0.5
 
 #Setup a TCP-Tahoe connection
 #Using Agent/TCP creates a Tahoe connection
@@ -42,14 +48,14 @@ $ns attach-agent $n0 $tcp_1
 
 #Create the sink for the connection
 set sink_1 [new Agent/TCPSink]
-$ns attach-agent $n1 $sink_1
+$ns attach-agent $n2 $sink_1
 #Establish the connection
 $ns connect $tcp_1 $sink_1
 
 #Set other properties
-$tcp_1 set fid_ 1
-$tcp_1 set window_ 6000
 #default packet-size is 1000
+$tcp_1 set fid_ 1
+$tcp_1 set window_ 20
 $tcp_1 set packetSize_ 1200  
 
 #Setup a FTP over TCP connection
@@ -57,37 +63,15 @@ set ftp_1 [new Application/FTP]
 $ftp_1 attach-agent $tcp_1
 $ftp_1 set type_ FTP
 
-#Setup a TCP-Tahoe connection
-set tcp_2 [new Agent/TCP]
-$tcp_2 set class_ 2
-#Set the source node
-$ns attach-agent $n0 $tcp_2
-
-#Create the sink for the connection
-set sink_2 [new Agent/TCPSink]
-#Set the sink node
-$ns attach-agent $n1 $sink_2
-#Establish the connection
-$ns connect $tcp_2 $sink_2
-
-#Set other properties
-$tcp_2 set fid_ 2
-$tcp_2 set window_ 6000
-$tcp_2 set packetSize_ 900
-
-#Setup a FTP over TCP connection
-set ftp_2 [new Application/FTP]
-$ftp_2 attach-agent $tcp_2
-$ftp_2 set type_ FTP
-
-#Schedule events for the two TCP agents
+#Schedule events for the TCP agent
 $ns at 0.5 "$ftp_1 start"
-$ns at 1.0 "$ftp_2 start"
-$ns at 3.0 "$ftp_2 stop"
-$ns at 3.5 "$ftp_1 stop"
+$ns at 5.0 "$ftp_1 stop"
 
-#Call the finish procedure after 4 seconds of simulation time
-$ns at 4.0 "finish"
+#Call the finish procedure after 5.5 seconds of simulation time
+$ns at 5.5 "finish"
 
 #Run the simulation
 $ns run
+
+#NOTE: TCP CANNOT be simulated as such. Requires a dummy-FTP Application
+#protocol to simulate and schedule
